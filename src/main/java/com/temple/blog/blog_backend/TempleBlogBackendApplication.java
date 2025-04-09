af3b3f92d12252660b34740d2a46d7d44c956982
Emil Nasinnikov <emilnsinnk@gmail.com>
2025-04-09 16:28:17 +0000

    Initial commit: Temple Blog project setup

---
+++ src/main/java/com/temple/blog/blog_backend/TempleBlogBackendApplication.java
@@ -0,0 +1,13 @@
+package com.temple.blog.blog_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TempleBlogBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TempleBlogBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/temple/blog/blog_backend/TempleBlogBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.temple.blog.blog_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TempleBlogBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
